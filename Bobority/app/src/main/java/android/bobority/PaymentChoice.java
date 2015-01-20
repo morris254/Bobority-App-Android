d40dc612306a4faf75d3f453040786e93b223933
cdemolde <CarlDemolder@gmail.com>
2015-01-20 01:32:07 -0800

    Revert "Revert "Added BrainTree integration. Pretty nifty. Need to still add Card.io separately though :/""
    This reverts commit 328dd12163f91e8448d2829cc5b54f2d8c896eee.

--- Bobority/app/src/main/java/android/bobority/PaymentChoice.java
+++ Bobority/app/src/main/java/android/bobority/PaymentChoice.java
@@ -12,6 +12,13 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 
+import com.braintreepayments.api.dropin.BraintreePaymentActivity;
+import com.braintreepayments.api.dropin.Customization;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+import com.loopj.android.http.TextHttpResponseHandler;
+
 import org.json.JSONException;
 
 import java.math.BigDecimal;
@@ -21,41 +28,76 @@ import java.util.Set;
 
 public class PaymentChoice extends Activity
 {
-    private static final String TAG = "paymentExample";
-
-    private ImageButton CardButton;
-
+    public static final int REQUEST_CODE = 100;
+    public String clientToken;
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paymentchoice);
+    }
 
-        CardButton = (ImageButton)findViewById(R.id.cardButton);
-        CardButton.setOnClickListener(new View.OnClickListener()
+    public void onBraintreeSubmit(View v)
+    {
+        /*AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://your-server/client_token", new TextHttpResponseHandler()
         {
-            public void onClick(View v)
+            public String clientToken;
+
+            @Override
+            public void onSuccess(String clientToken)
             {
-                Intent intent = new Intent(PaymentChoice.this, EnterCard.class);
-                startActivity(intent);
+                this.clientToken = clientToken;
             }
         });
+        Intent intent = new Intent(PaymentChoice.this, BraintreePaymentActivity.class);
+        intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN, this.clientToken);
+        Customization customization = new Customization.CustomizationBuilder()
+                .primaryDescription("Cart")
+                .secondaryDescription("3 Items")
+                .amount("$35")
+                .submitButtonText("Purchase")
+                .build();
+        intent.putExtra(BraintreePaymentActivity.EXTRA_CUSTOMIZATION, customization);
+        // REQUEST_CODE is arbitrary and is only used within this activity.
+        startActivityForResult(intent, REQUEST_CODE);*/
+        Intent intent = new Intent(PaymentChoice.this, BraintreePaymentActivity.class);
+        intent.putExtra(BraintreePaymentActivity.EXTRA_CLIENT_TOKEN, "eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiI3ZTQ2MDQ1ZTI3NWY3MWQxNGRhZmJiODc0YmRhZmExZWMwNjhhZGJjMjM3MDJmYWI4ODRkZmI3ODQ4YmFjODhlfGNyZWF0ZWRfYXQ9MjAxNS0wMS0yMFQwODo0MjozMy43MTEzNzQ0NDMrMDAwMFx1MDAyNm1lcmNoYW50X2lkPWRjcHNweTJicndkanIzcW5cdTAwMjZwdWJsaWNfa2V5PTl3d3J6cWszdnIzdDRuYzgiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvZGNwc3B5MmJyd2RqcjNxbi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwicGF5bWVudEFwcHMiOltdLCJjbGllbnRBcGlVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvZGNwc3B5MmJyd2RqcjNxbi9jbGllbnRfYXBpIiwiYXNzZXRzVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhdXRoVXJsIjoiaHR0cHM6Ly9hdXRoLnZlbm1vLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhbmFseXRpY3MiOnsidXJsIjoiaHR0cHM6Ly9jbGllbnQtYW5hbHl0aWNzLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb20ifSwidGhyZWVEU2VjdXJlRW5hYmxlZCI6dHJ1ZSwidGhyZWVEU2VjdXJlIjp7Imxvb2t1cFVybCI6Imh0dHBzOi8vYXBpLnNhbmRib3guYnJhaW50cmVlZ2F0ZXdheS5jb206NDQzL21lcmNoYW50cy9kY3BzcHkyYnJ3ZGpyM3FuL3RocmVlX2Rfc2VjdXJlL2xvb2t1cCJ9LCJwYXlwYWxFbmFibGVkIjp0cnVlLCJwYXlwYWwiOnsiZGlzcGxheU5hbWUiOiJBY21lIFdpZGdldHMsIEx0ZC4gKFNhbmRib3gpIiwiY2xpZW50SWQiOm51bGwsInByaXZhY3lVcmwiOiJodHRwOi8vZXhhbXBsZS5jb20vcHAiLCJ1c2VyQWdyZWVtZW50VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3RvcyIsImJhc2VVcmwiOiJodHRwczovL2Fzc2V0cy5icmFpbnRyZWVnYXRld2F5LmNvbSIsImFzc2V0c1VybCI6Imh0dHBzOi8vY2hlY2tvdXQucGF5cGFsLmNvbSIsImRpcmVjdEJhc2VVcmwiOm51bGwsImFsbG93SHR0cCI6dHJ1ZSwiZW52aXJvbm1lbnROb05ldHdvcmsiOnRydWUsImVudmlyb25tZW50Ijoib2ZmbGluZSIsIm1lcmNoYW50QWNjb3VudElkIjoic3RjaDJuZmRmd3N6eXR3NSIsImN1cnJlbmN5SXNvQ29kZSI6IlVTRCJ9LCJjb2luYmFzZUVuYWJsZWQiOmZhbHNlLCJtZXJjaGFudElkIjoiZGNwc3B5MmJyd2RqcjNxbiIsInZlbm1vIjoib2ZmbGluZSIsImFwcGxlUGF5Ijp7InN0YXR1cyI6Im1vY2siLCJjb3VudHJ5Q29kZSI6IlVTIiwiY3VycmVuY3lDb2RlIjoiVVNEIiwibWVyY2hhbnRJZGVudGlmaWVyIjoibWVyY2hhbnQuY29tLmJyYWludHJlZXBheW1lbnRzLmRldi1kY29wZWxhbmQiLCJzdXBwb3J0ZWROZXR3b3JrcyI6WyJ2aXNhIiwibWFzdGVyY2FyZCIsImFtZXgiXX19");
+        startActivityForResult(intent, REQUEST_CODE);
     }
 
-
-    /*
-     * Enable retrieval of shipping addresses from buyer's PayPal account
-     */
-    /*private void enableShippingAddressRetrieval(PayPalPayment paypalPayment, boolean enable)
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
     {
-        paypalPayment.enablePayPalShippingAddressesRetrieval(enable);
+        if (requestCode == REQUEST_CODE)
+        {
+            switch (resultCode)
+            {
+                case BraintreePaymentActivity.RESULT_OK:
+                    String paymentMethodNonce = data.getStringExtra(BraintreePaymentActivity.EXTRA_PAYMENT_METHOD_NONCE);
+                    postNonceToServer(paymentMethodNonce);
+                    break;
+                case BraintreePaymentActivity.BRAINTREE_RESULT_DEVELOPER_ERROR:
+                case BraintreePaymentActivity.BRAINTREE_RESULT_SERVER_ERROR:
+                case BraintreePaymentActivity.BRAINTREE_RESULT_SERVER_UNAVAILABLE:
+                    // handle errors here, a throwable may be available in
+                    // data.getSerializableExtra(BraintreePaymentActivity.EXTRA_ERROR_MESSAGE)
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
-    @Override
-    public void onDestroy()
+    void postNonceToServer(String nonce)
     {
-        // Stop service when done
-        stopService(new Intent(this, PayPalService.class));
-        super.onDestroy();
-    }*/
+        AsyncHttpClient client = new AsyncHttpClient();
+        RequestParams params = new RequestParams();
+        params.put("nonce", nonce);
+        client.post("https://your-server/payment-method-nonce", params,
+                new AsyncHttpResponseHandler()
+                {
+                    // Your implementation here
+                }
+        );
+    }
 }
 
